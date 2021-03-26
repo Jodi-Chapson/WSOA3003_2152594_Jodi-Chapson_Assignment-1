b17072743f71900c7c4629f25d376c576fd59cac
Jodi Chapson <2152594@students.wits.ac.za>
2021-03-26 04:27:03 +0200

    Level created, Scene management scripts and collectible art assets added

---
+++ WSOA3003_Assignment 1/Assets/Scripts/Chara_Info.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chara_Info : MonoBehaviour
+{
+    public string UnitName;
+    public int level = 1; //is multiplier for all other stats.
+
+    public int type; //types: 0 = protag, 1 = base enemy, 2 = elemental enemy
+    public int element;  // 0 = none, 1 = fire, 2 = water
+
+
+    public int damage;
+
+
+    public int maxHP;
+    public int currentHP;
+
+    //public void Start()
+    //{
+    //    damage = damage * level;
+    //    maxHP = maxHP * level;
+    //}
+
+}
